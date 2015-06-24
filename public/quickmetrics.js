0b97fa2ff67f9da582e092ff32496c4a38233a0f
ckib16 <ckib16@gmail.com>
2015-06-23 22:14:04 -0400

    Updates from CBeck on JS

--- public/quickmetrics.js
+++ public/quickmetrics.js
@@ -1,15 +1,19 @@
 //QuickMetrics snippet to put in tracked application
 
 $(document).ready( function(){
-  var _bm_event = {
-    event: { name: "pageLoad"},
+  track: function(event_name) {
+    var _bm_event = {
+      event: { name: event_name},
+    }
+    var _bm_request = new XMLHttpRequest();
+    _bm_request.open("POST", "https://quickmetrics.herokuapp.com//api/events", true);
+    _bm_request.setRequestHeader('Content-Type', 'application/json');
+    _bm_request.onreadystatechange = function() {
+      // respond to controller finish events
+    };
+    _bm_request.send(JSON.stringify(_bm_event));
   }
 
-  var _bm_request = new XMLHttpRequest();
-  _bm_request.open("POST", "https://quickmetrics.herokuapp.com//api/events", true);
-  _bm_request.setRequestHeader('Content-Type', 'application/json');
-  _bm_request.onreadystatechange = function() {
-    // respond to controller finish events
-  };
-  _bm_request.send(JSON.stringify(_bm_event));
 });
+
+
