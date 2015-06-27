049a3bd47cf431160fc2b029d19976af534616f2
ckib16 <ckib16@gmail.com>
2015-06-27 15:34:48 -0400

    Snippet changed to BlocMetrics example
    Stuck on broken JS
    - pasted from Blocmetrics example to see if can get working
    - Fixed URL for QuickMetrics API

--- public/quickmetrics.js
+++ public/quickmetrics.js
@@ -1,17 +1,23 @@
 //QuickMetrics snippet to put in tracked application
 
 $(document).ready( function(){
-  track: function(event_name) {
-    var _bm_event = {
-      event: { name: event_name},
+
+  var Blocmetrics = {
+    authToken: null,
+    setAuthToken: function(auth_token) {
+      this.authToken = auth_token;
+    },
+
+    send: function(name, data) {
+      var _bm_request = new XMLHttpRequest();
+      _bm_request.open("POST", "https://quickmetrics.herokuapp.com//api/events", true);
+      _bm_request.setRequestHeader('Content-Type', 'application/json');
+      _bm_request.onreadystatechange = function() {
+        // this function runs when the Ajax request changes state.
+        // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+      };
+      _bm_request.send(JSON.stringify({auth_token: this.authToken, name: name, data: data}));
     }
-    var _bm_request = new XMLHttpRequest();
-    _bm_request.open("POST", "https://quickmetrics.herokuapp.com//api/events", true);
-    _bm_request.setRequestHeader('Content-Type', 'application/json');
-    _bm_request.onreadystatechange = function() {
-      // respond to controller finish events
-    };
-    _bm_request.send(JSON.stringify(_bm_event));
   }
 
 });
