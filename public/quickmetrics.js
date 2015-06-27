818f173effd360e7216bab193372c343b4ac70c0
ckib16 <ckib16@gmail.com>
2015-06-27 15:31:12 -0400

    Revert "JQuery still not working"
    This reverts commit f5474c98af825cd98ebbee65a1902d03a773e31c.

--- public/quickmetrics.js
+++ public/quickmetrics.js
@@ -1,16 +1,19 @@
-var Blocmetrics = {
-  authToken: null,
-  setAuthToken: function(auth_token) {
-    this.authToken = auth_token;
-  },
-  send: function(name, data) {
+//QuickMetrics snippet to put in tracked application
+
+$(document).ready( function(){
+  track: function(event_name) {
+    var _bm_event = {
+      event: { name: event_name},
+    }
     var _bm_request = new XMLHttpRequest();
-    _bm_request.open("POST", "http://localhost:3000/api/v1/events.json", true);
+    _bm_request.open("POST", "https://quickmetrics.herokuapp.com//api/events", true);
     _bm_request.setRequestHeader('Content-Type', 'application/json');
     _bm_request.onreadystatechange = function() {
-      // this function runs when the Ajax request changes state.
-      // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+      // respond to controller finish events
     };
-    _bm_request.send(JSON.stringify({auth_token: this.authToken, name: name, data: data}));
+    _bm_request.send(JSON.stringify(_bm_event));
   }
-}
+
+});
+
+
